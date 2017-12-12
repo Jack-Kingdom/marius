d67ecdb0226d047a6470314f7f6d9de9122608bf
Jack King <email@qiaohong.org>
2017-12-12 17:50:29 +0800

    add test code

--- test/decr/every.py
+++ test/decr/every.py
@@ -1,5 +1,6 @@
 import time
 from sisyphus.core import Job, TimeLine
+from sisyphus.helper import every
 
 now = time.time()
 before = [now for _ in range(10)]
@@ -12,7 +13,10 @@ def func1(num):
 
 if __name__ == '__main__':
     tl = TimeLine()
-    tl.add(Job(iter([now + i for i in [2, 3, 5]]), func1, 3))
+    tl.add(Job(every(2), func1, 2))
+    tl.add(Job(every(3), func1, 3))
+    tl.add(Job(every(4), func1, 4))
+
     while tl.has_jobs():
         tl.wait_next()
         tl.run()
